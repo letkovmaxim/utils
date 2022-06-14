c911fc7d907288fa295ba5f2c71d9279c6a691d1
letkovmaxim <79664117+letkovmaxim@users.noreply.github.com>
2022-06-14 20:48:54 +0300

    Add files via upload

---
+++ utils/src/main/java/App.java
@@ -0,0 +1,96 @@
+public class App {
+
+    public static void main(String[] args) {
+        ClassUtils.showInfo(new String());
+    }
+}
+
+//OUTPUT:
+//Пакет :
+//    java.util;
+//Название класса :
+//    ArrayList;
+//Родитель класса :
+//    AbstractList;
+//Интерфейсы класса :
+//    List;
+//    RandomAccess;
+//    Cloneable;
+//    Serializable;
+//Вложенные классы :
+//    ListItr;
+//    Itr;
+//    SubList;
+//    ArrayListSpliterator;
+//Поля класса :
+//    private static final long serialVersionUID;
+//    private static final int DEFAULT_CAPACITY;
+//    private static final Object[] EMPTY_ELEMENTDATA;
+//    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+//    transient Object[] elementData;
+//    private int size;
+//Конструкторы класса :
+//    public java.util.ArrayList(Collection arg0);
+//    public java.util.ArrayList();
+//    public java.util.ArrayList(int arg0);
+//Методы класса :
+//    public boolean add(Object arg0);
+//    private void add(Object arg0, Object[] arg1, int arg2);
+//    public void add(int arg0, Object arg1);
+//    public boolean remove(Object arg0);
+//    public Object remove(int arg0);
+//    public Object get(int arg0);
+//    public boolean equals(Object arg0);
+//    public int hashCode();
+//    public Object clone();
+//    public int indexOf(Object arg0);
+//    public void clear();
+//    public int lastIndexOf(Object arg0);
+//    public boolean isEmpty();
+//    public void replaceAll(UnaryOperator arg0);
+//    public int size();
+//    public List subList(int arg0, int arg1);
+//    public Object[] toArray();
+//    public Object[] toArray(Object[] arg0);
+//    public Iterator iterator();
+//    public boolean contains(Object arg0);
+//    public Spliterator spliterator();
+//    public boolean addAll(int arg0, Collection arg1);
+//    public boolean addAll(Collection arg0);
+//    public Object set(int arg0, Object arg1);
+//    private void readObject(ObjectInputStream arg0);
+//    private void writeObject(ObjectOutputStream arg0);
+//    public void forEach(Consumer arg0);
+//    public void sort(Comparator arg0);
+//    public void ensureCapacity(int arg0);
+//    public void trimToSize();
+//    Object elementData(int arg0);
+//    private Object[] grow();
+//    private Object[] grow(int arg0);
+//    int indexOfRange(Object arg0, int arg1, int arg2);
+//    int lastIndexOfRange(Object arg0, int arg1, int arg2);
+//    private void rangeCheckForAdd(int arg0);
+//    private void fastRemove(Object[] arg0, int arg1);
+//    private boolean equalsArrayList(ArrayList arg0);
+//    boolean equalsRange(List arg0, int arg1, int arg2);
+//    private void checkForComodification(int arg0);
+//    int hashCodeRange(int arg0, int arg1);
+//    private static String outOfBoundsMsg(int arg0, int arg1);
+//    private String outOfBoundsMsg(int arg0);
+//    private void shiftTailOverGap(Object[] arg0, int arg1, int arg2);
+//    boolean batchRemove(Collection arg0, boolean arg1, int arg2, int arg3);
+//    static Object elementAt(Object[] arg0, int arg1);
+//    boolean removeIf(Predicate arg0, int arg1, int arg2);
+//    public boolean removeIf(Predicate arg0);
+//    private static long[] nBits(int arg0);
+//    private static void setBit(long[] arg0, int arg1);
+//    private static boolean isClear(long[] arg0, int arg1);
+//    private void replaceAllRange(UnaryOperator arg0, int arg1, int arg2);
+//    protected void removeRange(int arg0, int arg1);
+//    public boolean removeAll(Collection arg0);
+//    public boolean retainAll(Collection arg0);
+//    public ListIterator listIterator();
+//    public ListIterator listIterator(int arg0);
+//    void checkInvariants();
+
+
---
+++ utils/src/main/java/ClassUtils.java
@@ -0,0 +1,110 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+
+public class ClassUtils {
+
+    public static void showInfo(Object obj) {
+        Class<?> cls = obj.getClass();
+
+        if (!cls.getPackageName().isEmpty()) {
+            System.out.println("Пакет :\n    " + cls.getPackageName() + ";");
+        }
+
+
+        System.out.println("Название класса :\n    " + cls.getSimpleName() + ";");
+
+
+        Annotation[] ann = cls.getDeclaredAnnotations();
+        if (ann.length != 0) {
+            System.out.println("Аннотации класса :");
+            for (Annotation an : ann) {
+                System.out.println("    " + an.annotationType().getSimpleName() + ";");
+            }
+        }
+
+
+        Class<?> superClass = cls.getSuperclass();
+        if (superClass != null) {
+            System.out.println("Родитель класса :\n    " + superClass.getSimpleName() + ";");
+        }
+
+
+        Class<?>[] intrf = cls.getInterfaces();
+        if (intrf.length != 0) {
+            System.out.println("Интерфейсы класса :");
+            for (Class<?> intr : intrf) {
+                System.out.println("    " + intr.getSimpleName() + ";");
+            }
+        }
+
+
+        Class<?>[] clzz = cls.getDeclaredClasses();
+        if (clzz.length != 0) {
+            System.out.println("Вложенные классы :");
+            for (Class<?> clz : clzz) {
+                System.out.println("    " + clz.getSimpleName() + ";");
+            }
+        }
+
+        Field[] fld = cls.getDeclaredFields();
+        if (fld.length != 0) {
+            System.out.println("Поля класса :");
+            for (Field fl : fld) {
+                if (fl.getModifiers() != 0) {
+                    System.out.println("    " + Modifier.toString(fl.getModifiers())
+                            + " " + fl.getType().getSimpleName()
+                            + " " + fl.getName() + ";");
+                } else {
+                    System.out.println("    " + fl.getType().getSimpleName()
+                            + " " + fl.getName() + ";");
+                }
+            }
+        }
+
+
+        Constructor<?>[] cnstr = cls.getDeclaredConstructors();
+        if (cnstr.length != 0) {
+            System.out.println("Конструкторы класса :");
+            for (Constructor<?> cnst : cnstr) {
+                if (cnst.getModifiers() != 0) {
+                    System.out.print("    " + Modifier.toString(cnst.getModifiers())
+                            + " " + cnst.getName());
+                } else {
+                    System.out.print("    " + cnst.getName());
+                }
+
+                String sep = "";
+                System.out.print("(");
+                for (Parameter param : cnst.getParameters()) {
+                    System.out.print(sep + param.getType().getSimpleName() + " " + param.getName());
+                    sep = ", ";
+                }
+                System.out.println(");");
+            }
+        }
+
+
+        Method[] mthd = cls.getDeclaredMethods();
+        if (mthd.length != 0) {
+            System.out.println("Методы класса :");
+            for (Method mth : mthd) {
+                if (mth.getModifiers() != 0) {
+                    System.out.print("    " + Modifier.toString(mth.getModifiers())
+                            + " " + mth.getReturnType().getSimpleName()
+                            + " " + mth.getName());
+                } else {
+                    System.out.print("    " + mth.getReturnType().getSimpleName()
+                            + " " + mth.getName());
+                }
+
+                String sep = "";
+                System.out.print("(");
+                for (Parameter param : mth.getParameters()) {
+                    System.out.print(sep + param.getType().getSimpleName() + " " + param.getName());
+                    sep = ", ";
+                }
+                System.out.println(");");
+            }
+        }
+    }
+}
